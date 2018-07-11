e150d899ec6298732c730b2a24cc15a5e789bece
diana ORTEGA <dicaormu@yahoo.com>
2018-07-11 22:59:33 +0200

    jupyter notebooks: linear regression notebook

--- notebookUtils/LoadingUtils.go
+++ notebookUtils/LoadingUtils.go
@@ -2,32 +2,12 @@ package notebookUtils
 
 import (
 	"os"
-	"encoding/csv"
+	"io/ioutil"
 )
 
-func LoadCsvDataset(path string) ([][]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return [][]string{}, err
-	}
-	csvFile := csv.NewReader(file)
-	records, err := csvFile.ReadAll()
-	if err != nil {
-		return [][]string{}, err
-	}
-	file.Close()
-	return records, nil
-}
-
-func TransposeStringMatrix(matrix [][]string) [][]string {
-	r := make([][]string, len(matrix[0])) //39
-	for index := range r {
-		r[index] = make([]string, len(matrix))
-	}
-	for fv, f := range matrix {
-		for cv, c := range f {
-			r[cv][fv] = c
-		}
-	}
-	return r
+func GetGraph(graphName string) ([]byte, error) {
+	infile, err := os.Open(graphName)
+	bytes, err := ioutil.ReadAll(infile)
+	infile.Close()
+	return bytes, err
 }
